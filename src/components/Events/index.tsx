b7c374beedd42e2052b764a732ce8bb9ba3df3f5
Pawel <pawelpiatek98@gmail.com>
2022-12-03 08:38:31 +0100

    feat(CC-9): created add event modal

--- src/components/Events/index.tsx
+++ src/components/Events/index.tsx
@@ -1,22 +1,31 @@
 import { Typography } from 'antd';
 import Table from 'antd/es/table';
+import { useState } from 'react';
 import { AddItemButton, PageHeader, Spacer } from '../common';
+import { AddEventModal } from './components/AddEvent';
 import { Container } from './Events.styles';
 import { data, columns } from './mocks/eventsMock';
 
 const { Title } = Typography;
 
 const Events = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModalOpen = () => setIsModalOpen(true);
+
+  const handleModalClose = () => setIsModalOpen(false);
+
   return (
     <Container>
       <PageHeader>Wydarzenia</PageHeader>
-      <AddItemButton text="Utwórz wydarzenie" />
+      <AddItemButton text="Utwórz wydarzenie" onClick={handleModalOpen}/>
       <Spacer height={36} />
       {!data || data.length === 0 ? (
         <Title level={4}>Brak wydarzeń do wyświetlenia :(</Title>
       ) : (
         <Table columns={columns} dataSource={data} size="middle" pagination={false} />
       )}
+      <AddEventModal isOpen={isModalOpen} handleModalClose={handleModalClose} />
     </Container>
   );
 };
